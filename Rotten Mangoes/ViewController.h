5cf36d4dfd8e480e40916eb77e9e2b0ce7d0df7f
Jacob Cho <jacobshcho@gmail.com>
2014-10-29 09:38:00 -0700

    Initial Commit

---
+++ Rotten Mangoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Mangoes
+//
+//  Created by Jacob Cho on 2014-10-29.
+//  Copyright (c) 2014 Jacob Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
